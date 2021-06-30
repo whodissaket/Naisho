9bb2699a8f5f244ef083b50e25ece855d2fd9f8b
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2021-06-30 16:25:37 +0530

    Updated:Code formatting using prettier

--- app.js
+++ app.js
@@ -1,85 +1,181 @@
-const express = require('express');
-const ejs = require('ejs');
+//package initialization
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+//usual stuff
 const app = express();
-const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
-
-
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Creating session
+app.use(
+    session({
+        secret: "Mei nahi bataunga.",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+//Passport&Session Initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+//mongoDB connection to DB
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String,
+});
+//plugins
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+//Creating New model in DB
+const User = new mongoose.model("User", userSchema);
+passport.use(new LocalStrategy(User.authenticate()));
+
+//
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
 });
 
-const secret = "Secrethaiyehmeinnhibataunga";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
+//Google Oauth authentication
+passport.use(
+    new GoogleStrategy({
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/google/secrets",
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            User.findOrCreate({ googleId: profile.id }, function(err, user) {
+                return cb(err, user);
+            });
+        }
+    )
+);
 
-const User = new mongoose.model("User", userSchema);
-
+//GET Requests
 app.get("/", function(req, res) {
     res.render("home");
 });
-
+//Google Oauth GET Requests
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+//Callback for Google Oauth
+app.get(
+    "/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+        // Successful authentication, redirect secrets.
+        res.redirect("/secrets");
+    }
+);
 app.get("/login", function(req, res) {
-
     res.render("login");
-
 });
+
 app.get("/register", function(req, res) {
-
     res.render("register");
-
+});
+app.get("/secrets", function(req, res) {
+    User.find({ secret: { $ne: null } }, function(err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+app.get("/submit", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
 });
 
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 // Post Requests start here ....
 //RegisterRequest
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
+    User.register({ username: req.body.username },
+        req.body.password,
+        function(err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function() {
+                    res.redirect("/secrets");
+                });
+            }
         }
-    });
+    );
 });
 
 //LoginRequest
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username }, function(err, foundUser) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                };
-            };
-        };
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
     });
 });
 
-
-
-
-
-
-
-//
+//PORT
 let port = process.env.PORT;
+
 if (port == null || port == "") {
     port = 3000;
 }
